a3f77d1178e1986b1f1492c4f9605a92ae17cc1f
daflones <contatodaflunedu@gmail.com>
2026-03-13 16:43:17 -0300

    att

--- src/components/shared/SEOHead.jsx
+++ src/components/shared/SEOHead.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
-const SITE_URL = 'https://converthub.nanosync.com.br'
+const SITE_URL = 'https://nanosync.com.br'
 
 export default function SEOHead({
   title,
